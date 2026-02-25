cc4b9de699ec5383d4214b3dd8073fbe77876f4f
AnushSingla <singlaanush18@gmail.com>
2026-02-25 17:34:19 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ["https://jamtheweb.vercel.app/", "http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://jamtheweb.vercel.app", "http://localhost:5173", "http://localhost:5174"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
